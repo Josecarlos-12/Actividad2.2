878ab8ef83dfd524ae6b0c10e8034811fa70d754
daniel13524 <danielenrique.saves@hotmail.com>
2023-05-15 18:36:15 -0500

    Daniel Calderon_Enemigo1_movimiento izquierda - derecha

---
+++ Actividad2.2/Assets/Scripts/Enemys/Enemy1.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
+public class Enemy1 : MonoBehaviour
+{
+    [Header("Move")]
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private Transform[] points;
+    [SerializeField] private int destPoint = 0;
+    [SerializeField] private int distancePoint = 1;
+
+    [Header("Shot")]
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private float time, maxTime;
+    [SerializeField] private Transform shotPosition;
+
+    [SerializeField, Header("Life")] private int life;
+
+    [SerializeField, Header("Damage")] private int damage = 1;
+
+    private void Update()
+    {
+
+        if (agent.remainingDistance < distancePoint)
+        {
+            GoToNextPoint();
+        }
+    }
+
+    public void GoToNextPoint()
+    {
+        if (points.Length == 0)
+        {
+            return;
+        }
+
+        agent.destination = points[destPoint].position;
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+    void ChangeLife(int value)
+    {
+        life += value;
+
+        if (life <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public int GetDamage()
+    {
+        return damage;
+    }
+
+    /*private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<DamagePlayer>() != null)
+        {
+            ChangeLife(-other.gameObject.GetComponent<DamagePlayer>().GetDamagePlayer());
+            Destroy(other.gameObject);
+            print(life);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<DamagePlayer>() != null)
+        {
+            ChangeLife(-collision.gameObject.GetComponent<DamagePlayer>().GetDamagePlayer());
+            print(life);
+        }
+    }*/
+}
